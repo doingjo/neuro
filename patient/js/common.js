0226402556758971fe708dd1f9bf4e4973897b78
hojin <hojin0921@naver.com>
2021-11-16 07:50:21 +0900

    종합결과

--- patient/js/common.js
+++ patient/js/common.js
@@ -10,6 +10,17 @@
       return false;
     });
     /* faq */
+    /* 문답 내역 */
+    $('.inquire_list  .tn').on("click", function(e) {
+      if($(this).hasClass("on")){
+				$(this).removeClass("on").parent().find('.cn').slideUp();
+			}else{
+				$(this).addClass("on").parent().siblings().find('a').removeClass("on");
+				$(this).parent().find('.cn').slideDown().parent().siblings().find('.cn').slideUp();
+			}
+      return false;
+    });
+    /* 문답 내역 */
 
     /* 인풋박스 포커스 이벤트 */
     $('.input_default input, .input_default .form_select').focusin(function(){
@@ -137,12 +148,25 @@
     }      
     $(window).scroll(function() {headbg();});
     /* 페이지 헤드 스크롤에 따라 배경색 변경 */
-
     
-    var category_tab = new Swiper(".category_tab .swiper", {
+    new Swiper(".category_tab .swiper", {
       slidesPerView: "auto",
       spaceBetween: 0,
     });
+    new Swiper(".aotoswiper", {
+      slidesPerView: "auto",
+      spaceBetween: 0,
+    });
+    new Swiper(".aotoswiperc", {
+      slidesPerView: "auto",
+      spaceBetween: 0,
+      centeredSlides: true,
+    });
+    var dayWeek = new Swiper(".day_week", {
+      slidesPerView: 7,
+      spaceBetween: 0,
+    });
+    dayWeek.slideTo($('.day_week .swiper-slide').length,0)
 });
 
 /* 모달 팝업창 */
@@ -164,4 +188,294 @@ function modals_show(id){
 function resize(obj) {
   obj.style.height = "1px";
   obj.style.height = (12+obj.scrollHeight)+"px";
+}
+
+
+
+function mainBalanceData(data){
+  var overall_data = $('.overall_data')
+  var result = 100/data.all*data.value;
+  var chart = '<svg viewBox="0 0 32 32"><circle class="circle" r="16" cx="16" cy="16" style="stroke-dasharray: 10 100" /></svg>';
+  overall_data.find('.pie').html(chart).find('.circle').css('stroke-dasharray', result + ' 100');
+  overall_data.find('.num span').html(data.all)
+  overall_data.find('.num strong').html(data.value)
+}
+
+function bubble(data){
+  var diameter = 600,color = d3.scaleOrdinal(d3.schemeCategory20c);
+  var bubble = d3.pack().size([diameter, diameter]).padding(5);
+  var svg = d3.select('#bubble_chart').append('svg').attr('viewBox','0 0 ' + diameter + ' ' + diameter).attr('width', '100%').attr('height', '100%').attr('class', 'chart-svg');
+  var root = d3.hierarchy(data).sum(function(d) { return d.value; }).sort(function(a, b) { return b.value - a.value; });
+  bubble(root);
+  var node = svg.selectAll('.node').data(root.children).enter().append('g').attr('class', 'node').attr('transform', function(d) { return 'translate(' + d.x + ' ' + d.y + ')'; }).append('g').attr('class', 'graph');
+  node.append("circle").attr("r", function(d) { return d.r; }).style("fill", function(d) { 
+    return d.data.color; 
+  });
+  node.append("text").attr("dy", ".3em").style("font-weight", "bold").style("text-anchor", "middle")
+  .text(function(d) { 
+    return d.data.value + '회'; 
+  }).style("fill", "#ffffff").style("font-size", function(d) { return (d.r/11) + 'vw'; })
+  var tatgetTag = document.querySelector('.bubble_texts');
+  for (let index = 0; index < data.children.length; index++) {
+    let element = document.createElement('li');
+    let text = JSON.stringify(data.children[index].name).replace(/\"/gi, "");
+    let color = JSON.stringify(data.children[index].color).replace(/\"/gi, "");
+    element.innerHTML = "<span><i style='background:"+color+"'></i>"+text+"</span>";
+    tatgetTag.append(element);
+  }
+}
+
+function barchart(data, id, aver){
+  const bar_chart = document.getElementById(id)
+  var valdata = data.map(function(v){return v.value});
+  valdata= Math.max.apply(null,valdata);
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index].value;
+    if(aver){
+      var liHtml = `<li><strong>${data[index].value}점</strong><div class="b"><div class="r" style="height:${data[index].value}%"></div></div><span>${data[index].name}</span></li>`
+      bar_chart.innerHTML += liHtml;
+    }else{
+      const ratio = Math.floor(100/valdata*element)
+      var liHtml = `<li class=${ratio==100?'on':''}><strong>${data[index].value}회</strong><div class="b"><div class="r" style="height:${ratio}%"></div></div><span>${data[index].name}</span></li>`
+      bar_chart.innerHTML += liHtml;
+    }
+    const r = $('#'+id+' .b').eq(index).find('.r');
+    const ojli = $('#'+id+' li');
+    if(aver){
+      aver >= element+1 ? r.css("background-color","#e67700") : r.css("background-color","#5280e2");
+      if(data.length > 7){
+        const liwidth = ojli.outerWidth(true);
+        $('#'+id).width(liwidth*data.length);
+        $( window ).resize( function() {
+          const liwidth = ojli.outerWidth(true);
+          $('#'+id).width(liwidth*data.length);
+        });
+      }
+      const height = ojli.find('.b').height();
+      const top = ojli.position().top;
+      $('#'+id).parents('.averchart').find('.averline').height(height).css({'top':top+'px'}).find('.line').css({'bottom':aver+'%'}).parent().find('.tx').css({'bottom':aver+'%'});
+      $( window ).resize( function() {
+        const height = ojli.find('.b').height();
+        const top = ojli.position().top;
+        $('#'+id).parents('.averchart').find('.averline').height(height).css({'top':top+'px'}).find('.line').css({'bottom':aver+'%'}).parent().find('.tx').css({'bottom':aver+'%'});
+      });
+    }else{
+      const elementTop = r.position().top;
+      ojli.eq(index).find('strong').css({'top':elementTop+'px'});
+    }
+  }
+  $('#'+id+' .b').on("click", function(e) {
+    $('#'+id+' li').removeClass("on");
+    $(this).parent().addClass("on")
+  }); 
+}
+
+function probar(e, id, box){
+  sportsPoints = e/100,
+  svgSportsPath = document.getElementById(id);
+  var sports = new ProgressBar.Path(svgSportsPath, {
+    duration: 1000,
+    text: { value: '0' }
+  });
+  sports.set(sportsPoints);
+  var onebgHtml = `<div class="onebg"><div class="one"></div></div>`
+  var pinHtml = `<div class="pin"></div>`
+  var ruHtml = `<div class="ru"><ul></ul></div>`
+  $("#"+box).append(onebgHtml).append(pinHtml).append(ruHtml);
+  var per;
+  e < 60 ? per = e/10: per = e/10 + 1;
+  var perclass;
+  for (let index = 0; index < 12; index++) {
+    index - 1 < per ? perclass = 'on': perclass = 'off';
+    $("#"+box).find('.ru ul').append(`<li class="${perclass}"></li>`)
+  }
+  $("#"+box).find('.onebg').css('-webkit-transform','rotate('+e*1.8+'deg)'); 
+  $("#"+box).find('.pin').css('-webkit-transform','rotate('+e*1.8+'deg)'); 
+}
+
+
+function barCycle(data, id){
+  const bar_chart = document.getElementById(id);
+  var averNo = data.find(x => x.name === '표준').stance;
+  var averHtml = `<span class="averline" style="left:${averNo}%"></span>`
+    bar_chart.innerHTML += averHtml;
+  for (let index = 0; index < data.length; index++) {
+    var name = data[index].name, left = data[index].stance, right = data[index].swing, leftClass ='', color = '';
+    if(left >= 92 && left < 97){leftClass='f'}else if(left >= 97){leftClass='ff'}
+    if(name == '왼발'){color='left'}else if(name == '오른발'){color='right'}else{color='aver'}
+    var liHtml = `<div class="chart ${leftClass}  ${color}"><div class="bar"><span class="stance" style="width:${left}%"> </span></div><span class="stance_tx" style="left:${left}%">${left}</span><span class="swing_tx">${right}</span><span class="name">${name}</span></div>`
+    bar_chart.innerHTML += liHtml;
+  }
+}
+
+function balance(data){
+  var dataLength = data.length ;
+  var startDay = data[0].day -1;
+  var endDay = data[dataLength - 1].day;
+  const ojstag = document.getElementById('balancesvg');
+  ojstag.style.height = dataLength * 13 + "vw";
+  const ojs = document.getElementById('balance');
+  const width = ojs.offsetWidth;            
+  const height = ojs.offsetHeight - 20;
+  //function resizer(){
+  const yScale = d3.scaleLinear().domain([endDay, startDay]).range([10, height]);  
+  const xScale = d3.scaleLinear().domain([6, 0]).range([width, 10]);      
+  const yAxisSVG = d3.select("svg").append("g")//.select('text');
+  const yAxis = d3.axisRight(yScale).tickSize(0).ticks(dataLength);
+  yAxis(yAxisSVG);
+  const linearGenerator = d3.line()
+    .x(function(d, index) {
+      if(d.value == null || d.value == ''){
+        if(index == dataLength){
+          return xScale(data[index - 1].value)
+        }else{
+          return xScale(data[index + 1].value)
+        }
+      }else{
+        return xScale(d.value)
+      }
+    })
+    .y(function(d, index) {
+      if(d.value == null || d.value == ''){
+        if(index == dataLength){
+          return yScale(data[index - 1].day)
+        }else{
+          return yScale(data[index + 1].day)
+        }
+          return yScale(d.day)
+      }else{
+        return yScale(d.day)
+      }
+    });
+  d3.select("svg").data(data).selectAll(".tick").attr('class', function(d) {
+    var index = data.findIndex(i => i.day == d); 
+    if(index<0){return;}
+    if(data[index].value == null || data[index].value == ''){
+      return "nodata";
+    }else{
+      return 'tick';
+    }
+  }).selectAll("text").text(function(d) {
+    return (d < 10) ? ("0" + d) : d;
+})
+
+  d3.select("svg").append("path").attr("d", linearGenerator(data)).attr("fill", "none").attr("stroke-width", 4).attr("opacity", .1).attr("stroke", "#5280e3");
+  d3.select("svg").selectAll("circle").data(data).enter().append("circle").attr("r", 7)
+    .attr('cx', function(d, index) {
+      if(d.value == null || d.value == ''){
+        if(index == dataLength){
+          return xScale(data[index - 1].value)
+        }else{
+          return xScale(data[index + 1].value)
+        }
+      }else{
+        return xScale(d.value)
+      }
+    }).attr('cy', function(d, index) {
+      if(d.value == null || d.value == ''){
+        if(index == dataLength){
+          return yScale(data[index - 1].day)
+        }else{
+          return yScale(data[index + 1].day)
+        }
+      }else{
+        return yScale(d.day)
+      }
+    }).style("fill", "#fff").attr("stroke-width", 4).attr("stroke", "#5280e3");
+  // };
+  //   window.addEventListener('resize', resizer);
+  //   resizer();
+}
+
+function bachart(data, id){
+  const bachart = document.getElementById(id);
+  for (let index = data.length -1; index >= 0; index--) {
+    var nodata = ''
+    if(data[index].left == '' || data[index].left == null){ nodata='nodata'}
+    var liHtml = `<div class="ba_chart clear ${nodata}"><div class="day">${data[index].day}</div><div class="left bar"><span style="width:${data[index].left}%"></span></div><div class="right bar"><span style="width:${data[index].right}%"></span></div></div>`
+    bachart.innerHTML += liHtml;
+  }
+}
+
+
+function walk_tdata(data){
+  var dataLength = data.length ;
+  var startDay = data[0].day -0.5;
+  var endDay = data[dataLength - 1].day+0.5;
+  const ojstag = document.getElementById('walkTchart');
+  ojstag.style.width = dataLength * 11.5 + "vw";
+  const ojs = document.getElementById('walk_tchart');
+  const width = ojstag.offsetWidth;            
+  const height = ojs.offsetHeight;
+  const xScale = d3.scaleLinear().domain([startDay, endDay]).range([0, width]);
+  const yScale = d3.scaleLinear().domain([0, 3.3]).range([height - 50, 0]);
+  const xAxisSVG = d3.select("svg").append("g").attr("transform", "translate(0, "+height+")").attr('class', 'day');     
+  const xAxis = d3.axisBottom(xScale).tickSize(1).ticks(dataLength);
+  xAxis(xAxisSVG); 
+  d3.select("svg").selectAll(".domain").remove();
+  d3.select("svg").selectAll(".tick").attr('class', 'days');
+  d3.select("svg").call(d => d.selectAll('line').remove());
+  const linearGenerator = d3.line().x(d=>xScale(d.day)).y(d=>yScale(d.value));
+  d3.select("svg").append("path").attr("d", linearGenerator(data)).attr("fill", "none").attr("stroke-width", 4).attr("stroke", "#5280e2").attr("stroke-linecap", "round");
+  d3.select("svg").selectAll("circle").data(data).enter().append("circle").attr('class', 'cir').attr("r", 8).attr("cx", d=>xScale(d.day)).attr("cy", d=>yScale(d.value)).style("fill", "#5280e2").attr("stroke-width", 3).attr("stroke", "#fff")
+  const bar_chart = document.getElementById('weeklist')
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index].value;
+    var leftx = $('.days').eq(index).attr('transform').replaceAll("translate","").replace(/\)/g,'').replace(/\(/g,'').slice(0, -2);
+      var liHtml = `<li style="transform:translateX(${leftx}px)"><span class="liwa">${data[index].week}</span><span class="liday">${data[index].day}</span></li>`
+      bar_chart.innerHTML += liHtml;
+  }
+}
+
+function walk_on(index){
+  $('#weeklist li').eq(index).addClass("on").siblings().removeClass("on");
+  $('.cir').eq(index).addClass("on").siblings().removeClass("on");
+}
+
+
+function linechat(data, id, tx, index){
+  var dataLength = data.length ;
+  var startDay = data[0].day - 0.5;
+  var endDay = data[dataLength - 1].day + 0.5;
+  var valdata = data.map(function(v){return v.value});
+  valdata= Math.max.apply(null,valdata);
+  const ojstag = document.getElementById(id);
+  ojstag.style.width = dataLength * 11.5 + "vw";
+  const width = ojstag.offsetWidth;            
+  const height = ojstag.offsetHeight -30;
+  var svgoj = d3.select("#"+id).append("svg")
+  const xScale = d3.scaleLinear().domain([startDay, endDay]).range([0, width]);                
+  const yScale = d3.scaleLinear().domain([-5, valdata + 5]).range([height, 0]);
+  const xAxisSVG = svgoj.append("g").attr("transform", "translate(0, "+height+")");
+  const xAxis = d3.axisBottom(xScale).tickSize(1).ticks(dataLength);
+  xAxis(xAxisSVG);
+  svgoj.selectAll(".domain").remove();
+  svgoj.selectAll(".tick").attr('class', 'days');
+  svgoj.call(d => d.selectAll('line').remove());
+  const linearGenerator = d3.line().x(d=>xScale(d.day)).y(d=>yScale(d.value));
+  const linear = d3.line().x(d=>xScale(d.day));
+  svgoj.append("path").attr("d", linearGenerator(data)).attr("fill", "none").attr("stroke-width", 4).attr("stroke", "#e9ecef").attr("stroke-linecap", "round");
+  svgoj.append("g").selectAll("circle").data(data).enter().append("circle").attr('class', 'cir').attr("r", 6).attr("cx", d=>xScale(d.day)).attr("cy", d=>yScale(d.value)).style("fill", "#fff").attr("stroke-width", 3).attr("stroke", "#5280e2")
+
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index].value;
+    var leftx = $('#'+id+' .days').eq(index).attr('transform').replaceAll("translate","").replace(/\)/g,'').replace(/\(/g,'').slice(0, -2);
+    var topx = $('#'+id+' .cir').eq(index).attr('cy');
+    var liHtml = `<li style="transform:translateX(${leftx}px)"><span class="liday"><span>${data[index].day}</span></span></li>`
+    var liHtml1 = `<li style="transform:translate(${leftx}px, ${topx}px)"><span>${data[index].value}${tx}</span></li>`
+    $('#'+id+' .linechatli').append(liHtml)
+    $('#'+id+' .bar_tip').append(liHtml1)
+  }
+  $('#'+id+' .cir').on("click", function(e) {
+    var index = $(this).index();
+    lineclick(index)
+  }); 
+  $('#'+id+' .linechatli li').on("click", function(e) {
+    var index = $(this).index();
+    lineclick(index)
+  }); 
+  lineclick(index);
+  function lineclick(index){
+    $('#'+id+' .bar_tip li').eq(index).addClass("on").siblings().removeClass("on");
+  }
 }
