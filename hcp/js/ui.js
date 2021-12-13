10824c96f3c5080f3b74acf7c06529630d78da2d
DESKTOP-FAMF0HJ\cyk07 <cyk071@gmail.com>
2021-12-13 22:23:01 +0900

    홈 달력

--- hcp/js/ui.js
+++ hcp/js/ui.js
@@ -75,7 +75,7 @@ var hcp = hcp || {
         }
       });
     },
-  boardChk: function () {
+    boardChk: function () {
       if($(".tbl_notice label").length == 0){return;}
       $('.tbl_notice .check_type1 input[name=chk_ex]').on('click', function(){
         var target = $('.mod_body').find('.chk_ex');
@@ -145,6 +145,125 @@ var hcp = hcp || {
         }
       });
     },
+    fullcalendar: function(id, home, data){
+      var $id = $('#'+id),
+          headerToolbar = '',
+          fixedWeekCount = '';
+      if(home == 'home'){
+        headerToolbar = {
+          left: 'prev', center: 'title', right: 'next'
+        }
+        fixedWeekCount = true;
+      }else{
+        headerToolbar = {
+          left: 'title', right: 'prev,next'
+        }
+        fixedWeekCount = false;
+      }
+      var choiceDay = new Date();
+        document.addEventListener('DOMContentLoaded', function() {
+          var calendarEl = document.getElementById(id);
+          var calendar = new FullCalendar.Calendar(calendarEl, {
+            headerToolbar: headerToolbar,
+            footerToolbar: {
+              right: 'dayGridMonth,timeGridWeek,today'
+            },
+            fixedWeekCount: fixedWeekCount,
+            titleFormat : function(date) {
+              var month = '0'+(date.date.month +1);
+              return date.date.year +". "+month.slice(-2);
+            },
+            navLinks: true,
+            navLinkDayClick: function(date, jsEvent) {
+              // var year = date.getFullYear(),
+              //     month = parseInt(date.getMonth())+1,
+              //     day = date.getDate();
+              // $('.fc-day').removeClass('bg');
+              // //jsEvent.srcElement.classList.add('bg');
+              // jsEvent.srcElement.closest('.fc-day').classList.add('bg');
+
+              clickDay(date, jsEvent.srcElement)
+              calendar.gotoDate(date);
+              //console.log(year+'-'+month+'-'+day);
+            },
+            views: {
+              dayGridMonth: {
+                  dayHeaderContent: ({date}) => {
+                      var weekdays = ['일','월','화','수','목','금','토']
+                      return weekdays[date.getDay()]
+                  },
+              },
+              timeGridWeek: {
+                  dayHeaderContent: ({date}) => {
+                      var weekdays = ['일','월','화','수','목','금','토']
+                      return {html: `<div class="col-header">${weekdays[date.getDay()]}</div><div class="col-day col-day`+date.getDate()+`">${date.getDate()}</div>`}
+                  },
+              }
+            },
+            height: 'auto',
+            events: data,
+            datesSet:function(info) {
+              scheduleDayChange(choiceDay, choiceDay);
+            },
+          });
+          calendar.render();
+        });
+
+        function formatDate(date, yearIf){
+          var d = new Date(date),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+          if (month.length < 2)
+          month = '0' + month;
+          if (day.length < 2)
+          day = '0' + day;
+          if(yearIf){
+            return year+'-'+month+'-'+day;
+          }else{
+            return [month, day].join('.');
+          }
+        }
+        function clickDay(date, tag){
+          choiceDay = date;
+          scheduleDayChange(choiceDay, date);
+
+          console.log(formatDate(date,true));
+        }
+        function scheduleDayChange(choice, date){
+          $id.find('.bg').removeClass('bg');
+          $('.fc-daygrid-body .fc-day').each(function(i){
+            borne_sup = $(this).attr("data-date");
+            borne_choice = formatDate(choice,true);
+            if(borne_choice == borne_sup){
+              $(this).addClass('bg');
+            }
+          });
+          $('.fc-col-header .fc-day').each(function(i){
+            borne_sup = $(this).attr("data-date");
+            borne_choice = formatDate(choice,true);
+            if(borne_choice == borne_sup){
+              $('.time_wrap ul').eq(i).addClass('active').siblings().removeClass('active');
+            }
+          });
+          //오늘 버튼 on / off 필요하면 쓰고 // 안쓰면 생기는건,, 해당 달일때 이벤트 disable 걸림
+          ($('.fc-today-button').is(':disabled')) ? $('.btn_today').hide() : $('.btn_today').show() ;
+        }
+      
+        /* 월간, 주간 tridder */
+        $('.btn_today').click(function() {
+          $('.fc-today-button').trigger('click');
+        });
+        $('.btn_month').on('click', function(){
+          $('.time_wrap').hide();
+          $('.fc-dayGridMonth-button').trigger('click');
+        });
+        $('.btn_week').on('click', function(){
+          $('.time_wrap').fadeIn(300);
+          $('.fc-timeGridWeek-button').trigger('click');
+        });
+      
+    },//fullcalendar
     daterangepicker: function(ele, type, single){
       var $this = $(ele),
           clickDay = '';
