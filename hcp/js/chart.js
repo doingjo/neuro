59d5915dd5af09a43026df014ab128b2d1fbe047
DESKTOP-FAMF0HJ\cyk07 <cyk071@gmail.com>
2022-05-03 00:13:56 +0900

    HCP 디버깅 61번까지

--- hcp/js/chart.js
+++ hcp/js/chart.js
@@ -136,7 +136,7 @@ var chart = chart || {
         box.append('<p class="pin"></p>');
         $pin = (dataSet.value <= 100)? -(dataSet.value*.9) : (dataSet.value-100)*.9;
         $val = (dataSet.value <= 100)? dataSet.value : dataSet.value-100;
-        //box.append('<p class="txt_val">'+ $val +'</p>');
+        box.append('<p class="txt_val">'+ $val +'</p>');
         box.find('.pin').css('-webkit-transform','rotate('+ $pin +'deg)');
         box.css({'width': width, 'height': height});
         var svg = d3.select($this).append("svg").attr("width", width).attr("height", height);
